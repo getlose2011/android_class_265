c34589bd06d82dac3c80bd6ee091e059d1010772
Wuteyan <killerw74@gmail.com>
2016-05-12 21:52:32 +0800

    image loading and geocodingTask

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -162,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Order order = (Order) parent.getAdapter().getItem(position);
+                goToDetailOrder(order);
                 Snackbar.make(view, order.getNote(), Snackbar.LENGTH_SHORT).show();
             }
         });
@@ -202,6 +203,10 @@ public class MainActivity extends AppCompatActivity {
                     order.setNote(objects.get(i).getString("note"));
                     order.setStoreInfo(objects.get(i).getString("storeInfo"));
                     order.setMenuResults(objects.get(i).getString("menuResults"));
+                    if (objects.get(i).getParseFile("photo") != null)
+                    {
+                        order.photoURL = objects.get(i).getParseFile("photo").getUrl();
+                    }
                     orders.add(order);
 
                     if (results.size() <= i) {
@@ -289,6 +294,19 @@ public class MainActivity extends AppCompatActivity {
         intent.setClass(this, DrinkMenuActivity.class);
 
         startActivityForResult(intent, REQUEST_CODE_MENU_ACTIVITY);
+    }
+
+    public void goToDetailOrder(Order order)
+    {
+        Intent intent = new Intent();
+
+        intent.setClass(this, OrderDetailActivity.class);
+
+        intent.putExtra("note", order.getNote());
+        intent.putExtra("storeInfo", order.getStoreInfo());
+        intent.putExtra("menuResults", order.getMenuResults());
+        intent.putExtra("photoURL", order.photoURL);
+        startActivity(intent);
     }
 
     @Override
